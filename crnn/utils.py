df845319134dd1a0cf29e40593c3baad43d42208
obertcoy <laisobert2@gmail.com>
2024-12-14 16:21:08 +0700

    feat: added resize parameter

--- crnn/utils.py
+++ crnn/utils.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-def preprocess_image(image):
+def preprocess_image(image, resize=True):
 
     image_cv = image.astype(np.uint8)
     
@@ -9,7 +9,9 @@ def preprocess_image(image):
         image_cv = cv.cvtColor(image_cv, cv.COLOR_GRAY2BGR)
     elif image_cv.shape[2] == 4:
         image_cv = cv.cvtColor(image_cv, cv.COLOR_RGBA2BGR)
-    image_cv = cv.resize(image_cv, (0, 0), fx=2, fy=2)  
+
+    if(resize):
+        image_cv = cv.resize(image_cv, (0, 0), fx=2, fy=2)  
 
     denoised = cv.fastNlMeansDenoisingColored(image_cv, None, 10, 10, 7, 21)
 
